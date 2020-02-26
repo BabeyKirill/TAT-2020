1855be076eb5cd074e2799e4df5b1c88d066c284
BabeyKirill <kotonycta@gmail.com>
2020-02-27 00:39:59 +0300

    Initial commit

---
+++ DEV-1/DEV-1/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DEV_1
+{
+    /// <summary>
+    /// This programm count the maximum number of repeated consecutive characters in a string
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter the string: ");
+
+            while (true)
+            {
+                str = Console.ReadLine();
+                if (str == string.Empty)
+                {
+                    Console.WriteLine("The string should not be empty. Try again: ");
+                    continue;
+                }
+                break;
+            }
+
+            StringAnalyzer analyzer = new StringAnalyzer();
+            Console.WriteLine(analyzer.ToCountSameCharacters(str));
+        }
+    }
+}
---
+++ DEV-1/DEV-1/StringAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿namespace DEV_1
+{
+    /// <summary>
+    /// A class that contains methods for string analysis
+    /// </summary>
+    class StringAnalyzer
+    {
+        /// <summary>
+        /// Returns the maximum number of repeated consecutive characters in a string
+        /// </summary>
+        /// <param name="str">considered string</param>
+        public int ToCountSameCharacters(string str)
+        {
+            if (str.Length == 0)
+                return 0;
+
+            if (str.Length == 1)
+                return 1;
+
+            int count = 1;
+            int maxCount = 1;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i-1] == str[i])
+                {
+                    count++;
+
+                    if (maxCount < count)
+                        maxCount = count;
+                }
+                else
+                    count = 1;
+            }
+
+            return maxCount;
+        }
+    }
+}
